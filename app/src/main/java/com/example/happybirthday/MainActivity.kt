48edf7551fa40cca38a9e32d7ecae85e5d28ef3e
Ritej Belgacem <ritejbelqacem111@gmail.com>
2024-10-03 11:13:24 +0100

    Modifier la taille de la police

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +38,8 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
-
+        fontSize=100.sp,
+        lineHeight=116.sp,
     )
 }
 @Preview(showBackground = true)
